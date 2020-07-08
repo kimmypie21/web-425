f3ef4f4dd39167431d701bc0e3aa375f81929a73
Kimberly <kpierce865@yahoo.com>
2020-07-07 21:30:34 -0400

    commit

--- week-1/ts-examples/person-interface.ts
+++ week-1/ts-examples/person-interface.ts
@@ -1,3 +1,13 @@
+
+/*
+; Title:  Assignment 1.4
+; Author: Professor Krasso
+; Date: July 2020
+; Modified By: Kimberly Pierce
+; Description: TypeScript
+*/
+
+//create IPerson interface
 export interface IPerson {
     firstName: string;
     lastName: string;
--- week-1/ts-examples/person.class.ts
+++ week-1/ts-examples/person.class.ts
@@ -1,5 +1,15 @@
+/*
+; Title:  Assignment 1.4
+; Author: Professor Krasso
+; Date: July 2020
+; Modified By: Kimberly Pierce
+; Description: TypeScript
+*/
+
+//import IPerson interface from person-interface.ts
 import { IPerson } from "./person-interface";
 
+//create Person class
 class Person implements IPerson {
     firstName: string;
     lastName: string;
@@ -10,5 +20,6 @@ class Person implements IPerson {
     }
 }
 
+//create new Person 
 let myName = new Person ("Kimberly", "Pierce");
 console.log (`My name is ${myName.firstName} ${myName.lastName}`);
