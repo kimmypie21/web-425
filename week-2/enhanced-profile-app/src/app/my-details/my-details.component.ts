01b823c984b45cb89504eb94941afeea924ab653
Kimberly <kpierce865@yahoo.com>
2020-07-16 19:38:40 -0400

    commit

--- week-2/enhanced-profile-app/src/app/my-details/my-details.component.ts
+++ week-2/enhanced-profile-app/src/app/my-details/my-details.component.ts
@@ -11,14 +11,38 @@
 
 import { Component, OnInit } from '@angular/core';
 
+export default class Person {
+  fullName: string;
+  favoriteFood: string;
+  favoriteColor: string;
+  keywords = [
+    '#TypeScript ' , '#2020 ' , '#CodingWithAngular ' , '#ngOmaha'
+  ];
+  constructor( fullName: string, favoriteFood: string, favoriteColor: string){
+    this.fullName = fullName;
+    this.favoriteFood = favoriteFood;
+    this.favoriteColor = favoriteColor;
+  }
+
+  toString(){
+    console.log(`\n  Full name: ${this.fullName} \n  Favorite food: ${this.favoriteFood} \n  Favorite color: ${this.favoriteColor} `);
+  }
+}
+
 @Component({
   selector: 'app-my-details',
   templateUrl: './my-details.component.html',
   styleUrls: ['./my-details.component.css']
 })
+
 export class MyDetailsComponent implements OnInit {
 
-  constructor() { }
+  myProfile: Person;
+
+  constructor() {
+    this.myProfile = new Person ("Kimberly Pierce", "Carbs", "Purple");
+    this.myProfile.toString();
+  }
 
   ngOnInit(): void {
   }
